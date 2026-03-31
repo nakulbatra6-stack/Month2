c7b3d80ac482bc6b2033c87749110fd685617dd1
nakulbatra <batran737@gmail.com>
2026-03-31 21:34:51 +0530

    SeachAs

---
+++ SemanticSearchAssistant.py
@@ -0,0 +1,13 @@
+
+
+
+data = [
+    "I love machine learning",
+    "AI is fascinating",
+    "I enjoy playing football",
+    "Deep learning is powerful"
+]
+
+sen = input("Enter Sentence ")
+print(sen)
+
